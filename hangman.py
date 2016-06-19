5b1e8e3a94d39345571069fb48cbf843592949e0
karthikkalidas <karthik.kalidas@gmail.com>
2016-06-19 22:37:09 +0530

    Basic Functionality Implemented. Words.txt Added.

--- hangman.py
+++ hangman.py
@@ -1,7 +1,18 @@
-import time
 import sys
+from random import randint
 
-for i in range(5):
-    print i
-    sys.stdout.flush()
-    time.sleep(1)
+#Reading words from file and storing into a list
+with open("Words.txt", 'r') as WordsListFile:
+    WordsList = [line.rstrip('\n') for line in WordsListFile]
+
+#Taking no. of words
+ListLength=len(WordsList)
+#Getting a random word from list
+TheWord=WordsList[randint(0,ListLength-1)]
+TheWordLength=len(TheWord)
+print(TheWord)
+
+for i in range(0,TheWordLength):
+	print('_',end=" ")
+
+
