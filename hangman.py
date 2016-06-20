c2f5c498a2dbe0283f133e338ffa28835c55f530
karthikkalidas <karthik.kalidas@gmail.com>
2016-06-20 19:07:39 +0530

    Game Works.Multiple characters restricted. Simple use of lists

--- hangman.py
+++ hangman.py
@@ -1,5 +1,8 @@
+from __future__ import print_function
 import sys
+import subprocess
 from random import randint
+
 
 #Reading words from file and storing into a list
 with open("Words.txt", 'r') as WordsListFile:
@@ -8,11 +11,42 @@ with open("Words.txt", 'r') as WordsListFile:
 #Taking no. of words
 ListLength=len(WordsList)
 #Getting a random word from list
-TheWord=WordsList[randint(0,ListLength-1)]
+TheWord=WordsList[randint(0,ListLength-1)].upper()
 TheWordLength=len(TheWord)
-print(TheWord)
+print(TheWord+'\n')
 
-for i in range(0,TheWordLength):
-	print('_',end=" ")
+CurWord =['_']*TheWordLength
+Used=[]
+HangList=list('|HANGMAN')
+HangIndex=0
+
+
+while True:
+
+	if ''.join(CurWord)==TheWord:
+		print("Won")
+		break	
+
+	Letter=input('Guess A Letter: ').upper()
+
+	if Letter in TheWord:
+		LetterIndex=TheWord.index(Letter)
+		CurWord.pop(LetterIndex)
+		CurWord.insert(LetterIndex,Letter)
+
+
+	else:
+		Used.append(Letter)
+		HangList.pop(HangIndex)
+		HangIndex+=1
+		HangList.insert(HangIndex,'|')
+
+	print('Word: ',end="")
+	print(*CurWord, sep='')
+	print('Used: ',end="")
+	print(*Used, sep='')
+	print(*HangList, sep='')
+
+
 
 
